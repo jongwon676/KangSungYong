1b2fa18f4a4e835de3139cedcc4c6a3014ecb95f
jongwon676 <jongwon676@naver.com>
2015-10-16 09:32:07 +0900

    factory pattern homework

--- src/com/j2/factory/caseExtension/factorymethod/PizzaTestDrive.java
+++ src/com/j2/factory/caseExtension/factorymethod/PizzaTestDrive.java
@@ -1,7 +1,7 @@
 package com.j2.factory.caseExtension.factorymethod;
 public class PizzaTestDrive {
   public static void main(String[] args) {
-    PizzaStore nystore=new NYPizzaStore();
+    PizzaStore smstore=new SMPizzaStore();
     Pizza pizza=nystore.orderPizza("cheese");
     System.out.println("We ordered a "+pizza.getName());
   }
